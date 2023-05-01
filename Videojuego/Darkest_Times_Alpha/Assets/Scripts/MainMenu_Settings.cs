3f608a023c2eb4a93058800b6a62a13495345699
¨DavidMAC¨ <dm39988@gmail.com>
2023-05-01 17:21:46 -0600

    Menu inicial provisional

---
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/MainMenu_Settings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_Settings : MonoBehaviour
+{
+    public void StartGame(){
+        //SceneManager.LoadScene(); //Nombre o indice de la escena
+    }
+
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
