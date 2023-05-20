492b86fa981b13923bd5acf596abd5aef0566b01
David Med <104374797+DavMDTheThird@users.noreply.github.com>
2023-05-20 10:28:10 -0600

    Spawn point (#15)
    * Agregue basicamente el prefab de Spawn
    
    * Merge de lobby_alpha y spawnPoint
    
    ---------
    
    Co-authored-by: ¨DavidMAC¨ <¨dm39988@gmail.com¨>

--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/CHARACTER/Player_basic.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/CHARACTER/Player_basic.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player_basic{
     private short _hp;
+    private short _maxhp;
+    private short _lvl;
+    private short _xp;
     private short _lck;
     private short _atk;
     private short _spd;
@@ -17,6 +20,30 @@ public class Player_basic{
         }
         set{
             _hp = value;
+        }
+    }
+    public short MAXHP{
+        get{
+            return _maxhp;
+        }
+        set{
+            _maxhp = value;
+        }
+    }
+    public short LVL{
+        get{
+            return _lvl;
+        }
+        set{
+            _lvl = value;
+        }
+    }
+    public short XP{
+        get{
+            return _xp;
+        }
+        set{
+            _xp = value;
         }
     }
     public short LCK{
@@ -67,20 +94,26 @@ public class Player_basic{
             _money = value;
         }
     }
-    
+
     /// <summary>
     /// Constructor de jugador basico.
     /// </summary>
     /// <param name="HP">First value</param>
-    /// <param name="LUCK">Second value</param>
+    /// <param name="MAXHP">First value</param>
+    /// <param name="Level">First value</param>
+    /// <param name="xp">First value</param>
+    /// <param name="Luck">Second value</param>
     /// <param name="Atack">Third value</param>
     /// <param name="Speed">Fourth value</param>
     /// <param name="Stamina">Fifth value</param>
     /// <param name="Inventory">Sixth value</param>
     /// <param name="Money">Seventh value</param>
     /// <returns> Crea un jugador base</returns>
-    public Player_basic(short hp, short lck, short atk, short spd, short stamina, short inventory, short money){
+    public Player_basic(short hp, short maxhp, short lvl, short xp, short lck, short atk, short spd, short stamina, short inventory, short money){
         HP = hp;
+        MAXHP = maxhp;
+        LVL = lvl;
+        XP = xp;
         LCK = lck;
         ATK = atk;
         SPD = spd;
@@ -91,7 +124,8 @@ public class Player_basic{
 
 
     public void Info(){
-        Debug.Log("Health is " + HP);
+        Debug.Log("Health is " + HP + " out of " + MAXHP);
+        Debug.Log("Current level: " + LVL + " next level at " + XP + " xp");
         Debug.Log("Luck is " + LCK);
         Debug.Log("Atack is " + ATK);
         Debug.Log("Speed is " + SPD);
