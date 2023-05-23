4fd2b8bfb9bbc4fae8095d0d35a424ae10ddfec0
Saikou17 <a01783208@itesm.mx>
2023-05-23 16:05:35 -0600

    Tutorial y Hospital V3

--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/Armas_Luz.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/Armas_Luz.cs
@@ -1,42 +1,70 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
 
-// public class Armas_Luz : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-// {
-//     Mesh mesh = new Mesh(); // Crea un nuevo objeto Mesh
-//     GetComponent<MeshFilter>().mesh = mesh;
+public class Armas_Luz : MonoBehaviour
+{
+    private Mesh mesh;
+    void Start(){
 
-//     float fov = 90f;
-//     Vector3 origin = Vector3.zero;
-//     int rayCount = 2;
-//     float angle = 0f;
-//     float angleIncrease = fov / rayCount;
-//     float viewDistance = 50f;
+        mesh = new Mesh(); // Crea un nuevo objeto Mesh
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    private void Update(){
+
+        float fov = 20f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 50;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 5f;
 
 
-//     Vector3[] vertices = new Vector3[rayCount+1+1]; // Arreglo para almacenar las posiciones de los vértices
-//     Vector2[] uv = new Vector2[vertices.Length]; // Arreglo para almacenar las coordenadas UV (mapeo de texturas)
-//     int[] triangles = new int[rayCount*3]; // Arreglo para almacenar los índices de los triángulos
+        Vector3[] vertices = new Vector3[rayCount+1+1]; // Arreglo para almacenar las posiciones de los vértices
+        Vector2[] uv = new Vector2[vertices.Length]; // Arreglo para almacenar las coordenadas UV (mapeo de texturas)
+        int[] triangles = new int[rayCount*3]; // Arreglo para almacenar los índices de los triángulos
 
-//     vertices[0] = origin; // Establece la posición del primer vértice en (0, 0, 0)
-    
-//     for(int i=0; i<= rayCount; i++0){
-//         Vector3 vertex = origin + UtilsClass.getan
-//     }
-
-//     mesh.vertices = vertices; // Asigna los vértices al objeto Mesh
-//     mesh.uv = uv; // Asigna las coordenadas UV al objeto Mesh
-//     mesh.triangles = triangles; // Asigna los triángulos al objeto Mesh
-// }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
+        vertices[0] = origin; // Establece la posición del primer vértice en (0, 0, 0)
         
-//     }
-// }
+        int vertexIndex=1;
+        int triangleIndex = 0;
+        for(int i=0; i<= rayCount; i++  ){
+            Vector3 vertex;        
+            RaycastHit2D raycast2d = Physics2D.Raycast(origin,UtilsClass.GetVectorFromAngle(angle),viewDistance);
+            if(raycast2d.collider == null){
+                //No choca con ningun objeto
+                vertex = origin + UtilsClass.GetVectorFromAngle(angle) * viewDistance;
+            } else {
+                //Choca con las estructuras
+                vertex = raycast2d.point;
+            }
+            vertices[vertexIndex] = vertex;
+            if(i>0){
+                triangles[triangleIndex+0] = 0;
+                triangles[triangleIndex+1] = vertexIndex - 1;
+                triangles[triangleIndex+2] = vertexIndex;
+                triangleIndex += 3;
+            }
+            vertexIndex ++;
+            angle -= angleIncrease;
+        }
+        mesh.vertices = vertices; // Asigna los vértices al objeto Mesh
+        mesh.uv = uv; // Asigna las coordenadas UV al objeto Mesh
+        mesh.triangles = triangles; // Asigna los triángulos al objeto Mesh
+
+
+        // public void SetOrigin(Vector3 origin){
+        //     this.origin = origin;
+
+        // }
+
+        // public void SetAimDirection(Vector3 aimDirection){
+        //     startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) - fov / 2f;
+        // }
+    }
+
+
+}
+
--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/Vision_Mouse.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/Vision_Mouse.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Vision_Mouse : MonoBehaviour
 {
+    private Animator animator;
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = (Mathf.Atan2(targetDir.y , targetDir.x)*Mathf.Rad2Deg) - 90f;
-        transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+        Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //Guarda las el vector que se genera desde la camara hasta la posicion del cursor
+        float angle = (Mathf.Atan2(targetDir.y , targetDir.x)*Mathf.Rad2Deg) - 90f; //Obtiene el angulo tangente que se forma por el vector targetDir en grados
+        Debug.Log(angle);
+        // if(angle>=315 and angle<=45){
+        //     animator.SetFloat("Horizontal",1);
+        //     animator.SetFloat("Vertical",0);
+        // }
+        // if(angle>=45 and angle<=135){}
+        // if(angle>=135 and angle<=225){}
+        // if(angle>=225 and angle<=315){}
+        // transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
     }
 }
