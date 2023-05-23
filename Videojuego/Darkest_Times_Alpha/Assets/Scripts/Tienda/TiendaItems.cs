ad7a6b3b32a4ab54745bd977dd7589d7cacabe47
DANIEL SOULT GÓMEZ <105379375+sltDaniel@users.noreply.github.com>
2023-05-23 16:36:50 -0600

    Consumibles fix (#21)
    * Sistema de dialogos
    
    * Son los díalogos que se despliegan y los de opciones
    
    * Asset de NPC mueble
    
    * Avance de tienda
    
    * TimelineSettings

---
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/Tienda/TiendaItems.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiendaItems : MonoBehaviour
+{
+    public string nombreItem;
+    public int precioCompraItem;
+    public int precioVentaItem;
+
+    public GameObject itemToAdd;
+    public int amountToAdd;
+}
