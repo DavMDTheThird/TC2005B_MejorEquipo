1286440721c670efffc7ee91621de1d1d0d11187
Saikou17 <a01783208@itesm.mx>
2023-05-24 14:47:31 -0600

    Sistema Luz

--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/Armas_Luz.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/Armas_Luz.cs
@@ -5,25 +5,29 @@ using CodeMonkey.Utils;
 
 public class Armas_Luz : MonoBehaviour
 {
-    private Mesh mesh;
+    [SerializeField] private LayerMask layer;
+    private Mesh mesh; //Creamos una malla privada
+    private float fov;
+    private Vector3 origin;
+    private float startingAngle;
+
     void Start(){
 
-        mesh = new Mesh(); // Crea un nuevo objeto Mesh
-        GetComponent<MeshFilter>().mesh = mesh;
+        mesh = new Mesh(); // La malla utiliza varios vertices y areglo de triangulos
+        GetComponent<MeshFilter>().mesh = mesh; //Obtenemos el Mesh filter del game object y este referencia al mesh que creamos
+        fov = 20f; // Ancho de la vision
+        Vector3 origin = Vector3.zero; //Establece un vector con sus coordenadas en cero
     }
 
     private void Update(){
 
-        float fov = 20f;
-        Vector3 origin = Vector3.zero;
-        int rayCount = 50;
-        float angle = 0f;
-        float angleIncrease = fov / rayCount;
-        float viewDistance = 5f;
-
+        float viewDistance = 5f; // Largo de la vision
+        int rayCount = 50; // Cantidad de triangulos
+        float angle = startingAngle; //Inicializamos el angulo
+        float angleIncrease = fov / rayCount; // Angulo que se obtiene del ancho con la cantidad de triangulos  
 
         Vector3[] vertices = new Vector3[rayCount+1+1]; // Arreglo para almacenar las posiciones de los vértices
-        Vector2[] uv = new Vector2[vertices.Length]; // Arreglo para almacenar las coordenadas UV (mapeo de texturas)
+        Vector2[] uv = new Vector2[vertices.Length]; // Arreglo para almacenar las coordenadas UV (mapeo de texturas)   
         int[] triangles = new int[rayCount*3]; // Arreglo para almacenar los índices de los triángulos
 
         vertices[0] = origin; // Establece la posición del primer vértice en (0, 0, 0)
@@ -50,21 +54,19 @@ public class Armas_Luz : MonoBehaviour
             vertexIndex ++;
             angle -= angleIncrease;
         }
+
         mesh.vertices = vertices; // Asigna los vértices al objeto Mesh
         mesh.uv = uv; // Asigna las coordenadas UV al objeto Mesh
         mesh.triangles = triangles; // Asigna los triángulos al objeto Mesh
-
-
-        // public void SetOrigin(Vector3 origin){
-        //     this.origin = origin;
-
-        // }
-
-        // public void SetAimDirection(Vector3 aimDirection){
-        //     startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) - fov / 2f;
-        // }
     }
 
+    public void SetOrigin(Vector3 origin){
+            this.origin = origin;
+        }
+
+    public void SetAimDirection(Vector3 aimDirection){
+        startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) - fov / 2f;
+    }
 
 }
 
--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/Vision_Mouse.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/Vision_Mouse.cs
@@ -9,15 +9,7 @@ public class Vision_Mouse : MonoBehaviour
     void Update()
     {
         Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //Guarda las el vector que se genera desde la camara hasta la posicion del cursor
-        float angle = (Mathf.Atan2(targetDir.y , targetDir.x)*Mathf.Rad2Deg) - 90f; //Obtiene el angulo tangente que se forma por el vector targetDir en grados
-        Debug.Log(angle);
-        // if(angle>=315 and angle<=45){
-        //     animator.SetFloat("Horizontal",1);
-        //     animator.SetFloat("Vertical",0);
-        // }
-        // if(angle>=45 and angle<=135){}
-        // if(angle>=135 and angle<=225){}
-        // if(angle>=225 and angle<=315){}
-        // transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+        float angle = (Mathf.Atan2(targetDir.y , targetDir.x)*Mathf.Rad2Deg); //Obtiene el angulo tangente que se forma por el vector targetDir en grados
+        transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
     }
 }
