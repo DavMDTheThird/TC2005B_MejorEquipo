e25b74eb05d2283c81cce67c3f6d942d461a5bb9
DavidMDD <dm39988@gmail.com>
2023-05-26 00:21:01 -0600

    añadi la pagina web, y todo lo que conlleva. Falta BD.

---
+++ Pagina Web/app.js
@@ -0,0 +1,178 @@
+"use strict"
+
+import express from 'express'
+import mysql from 'mysql2/promise'
+import fs from 'fs'
+
+const app = express()
+const port = 5000
+
+app.use(express.json())
+app.use(express.static('./public'))
+
+async function connectToDB()
+{
+    return await mysql.createConnection({
+        host:'localhost',
+        user:'test_API',
+        password:'Hola_123',
+        database:'api_game_db'
+    })
+}
+
+app.get('/', (request,response)=>{
+    fs.readFile('./html/index.html', 'utf8', (err, html)=>{
+        if(err) response.status(500).send('There was an error: ' + err)
+        console.log('Loading page...')
+        response.send(html)
+    })
+})
+
+// Se necesita cambiar 
+app.get('/api/users', async (request, response)=>{
+    let connection = null
+
+    try
+    {
+        connection = await connectToDB()
+        const [results, fields] = await connection.execute('select * from users')
+
+        console.log(`${results.length} rows returned`)
+        console.log(results)
+        response.json(results)
+    }
+    catch(error)
+    {
+        response.status(500)
+        response.json(error)
+        console.log(error)
+    }
+    finally
+    {
+        if(connection!==null) 
+        {
+            connection.end()
+            console.log("Connection closed succesfully!")
+        }
+    }
+})
+
+app.get('/api/users/:id', async (request, response)=>
+{
+    let connection = null
+
+    try
+    {
+        connection = await connectToDB()
+
+        const [results_user, _] = await connection.query('select * from users where id_users= ?', [request.params.id])
+        
+        console.log(`${results_user.length} rows returned`)
+        response.json(results_user)
+    }
+    catch(error)
+    {
+        response.status(500)
+        response.json(error)
+        console.log(error)
+    }
+    finally
+    {
+        if(connection!==null) 
+        {
+            connection.end()
+            console.log("Connection closed succesfully!")
+        }
+    }
+})
+
+app.post('/api/users', async (request, response)=>{
+
+    let connection = null
+
+    try
+    {    
+        connection = await connectToDB()
+
+        const [results, fields] = await connection.query('insert into users set ?', request.body)
+        
+        console.log(`${results.affectedRows} row inserted`)
+        response.json({'message': "Data inserted correctly.", "id": results.insertId})
+    }
+    catch(error)
+    {
+        response.status(500)
+        response.json(error)
+        console.log(error)
+    }
+    finally
+    {
+        if(connection!==null) 
+        {
+            connection.end()
+            console.log("Connection closed succesfully!")
+        }
+    }
+})
+
+app.put('/api/users', async (request, response)=>{
+
+    let connection = null
+
+    try{
+        connection = await connectToDB()
+
+        const [results, fields] = await connection.query('update users set name = ?, surname = ? where id_users= ?', [request.body['name'], request.body['surname'], request.body['userID']])
+        
+        console.log(`${results.affectedRows} rows updated`)
+        response.json({'message': `Data updated correctly: ${results.affectedRows} rows updated.`})
+    }
+    catch(error)
+    {
+        response.status(500)
+        response.json(error)
+        console.log(error)
+    }
+    finally
+    {
+        if(connection!==null) 
+        {
+            connection.end()
+            console.log("Connection closed succesfully!")
+        }
+    }
+})
+
+app.delete('/api/users/:id', async (request, response)=>{
+
+    let connection = null
+
+    try
+    {
+        connection = await connectToDB()
+
+        const [results, fields] = await connection.query('delete from users where id_users= ?', [request.params.id])
+        
+        console.log(`${results.affectedRows} row deleted`)
+        response.json({'message': `Data deleted correctly: ${results.affectedRows} rows deleted.`})
+    }
+    catch(error)
+    {
+        response.status(500)
+        response.json(error)
+        console.log(error)
+    }
+    finally
+    {
+        if(connection!==null) 
+        {
+            connection.end()
+            console.log("Connection closed succesfully!")
+        }
+    }
+})
+
+app.listen(port, ()=>
+{
+    console.log(`App listening at http://localhost:${port}`)
+})
