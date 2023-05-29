2c6f9130eeeca849c4ebfaf814f1f1ea03b2ba2c
Saikou17 <a01783208@itesm.mx>
2023-05-29 13:18:25 -0600

    HospitalV1

---
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/Armas/Luz/LightWeapons.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightWeapons : MonoBehaviour
+{
+    // public int coolDownM; //EL cooldown max que tiene todas las armas
+    // public int coolDownA;
+    // public int coolDownL;
+    // // private int atkMechero = 5; //Atk del mechero
+    // // private int atkAntorcha = 10;// Atk de la antorcha
+    // // private int atkLinterna = 15;// Atk de la linterna
+    // // private int atkBengalas = 20;// Atk de las bemgalas]
+    // public bool IsBroken = false;// Booleano que verifica si esta apunto de romperse
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     coolDownM = 60;
+    //     coolDownA = 30;
+    //     coolDownL = 20;
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if(this.IsBroken == false && gameObject.tag == "mechero"){
+    //         coolDownM -= Time.deltaTime;
+    //     }
+    //     if(this.IsBroken == false && gameObject.tag == "antorcha"){
+    //         coolDownA -= Time.deltaTime;
+    //     }
+    //     if(this.IsBroken == false && gameObject.tag == "linterna"){
+    //         coolDownL -= Time.deltaTime;
+    //     }
+    //     if(coolDownM <= 0 && gameObject.tag == "mechero"){
+    //         this.IsBroken = true;
+    //         Destroy(gameObject);
+    //         Debug.Log("Se rompe");
+    //     }
+    //     if(coolDownA <= 0 && gameObject.tag == "antorcha"){
+    //         this.IsBroken = true;
+    //         Destroy(gameObject);
+    //         Debug.Log("Se rompe");
+    //     }
+    //     if(coolDownL <= 0 && gameObject.tag == "linterna"){
+    //         this.IsBroken = true;
+    //         Destroy(gameObject);
+    //         Debug.Log("Se rompe");
+    //     }
+        
+    // }
+}
