b75edeeaccde9b9d4352ec203c2d3fe87868bd1f
Saikou17 <a01783208@itesm.mx>
2023-06-05 19:44:19 -0600

    Organizar

--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/CHARACTER/Player_stats.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/CHARACTER/Player_stats.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class Player_stats : MonoBehaviour
 {
+    //API URI
+    [SerializeField] string url;
+    [SerializeField] string getUsersEP;
+    [SerializeField] Text errorText;
+
     //Player_basic playerBSC;
     public bool babyCharacter = true;
     Player_basic playerBSC = new Player_basic(10, 10, 0, 0, 50, 2, 4.5f, 5, 5, 0);
@@ -68,7 +75,35 @@ public class Player_stats : MonoBehaviour
         playerBSC.Money += money;
         Debug.Log(transform.name + " gain: " + money + " money. Total money now: " + playerBSC.Money);
     }
+    public void QueryCheckpoint()
+    {
+        StartCoroutine(GetCheckpoint());
+    }
 
+    public void InsertNewCheckpoint()
+    {
+        StartCoroutine(AddCheckpoint());
+    }
+    IEnumerator GetCheckpoint()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(url + getUsersEP))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success) {
+                //Debug.Log("Response: " + www.downloadHandler.text);
+                // Compose the response to look like the object we want to extract
+                // https://answers.unity.com/questions/1503047/json-must-represent-an-object-type.html
+                string jsonString = "{\"api\"juego\"checkpoint\":" + www.downloadHandler.text + "}";
+                allUsers = JsonUtility.FromJson<UserList>(jsonString);
+                //Aqui se realiza la funcionalidad
+                if (errorText != null) errorText.text = "";
+            } else {
+                Debug.Log("Error: " + www.error);
+                if (errorText != null) errorText.text = "Error: " + www.error;
+            }
+        }
+    }
 
 
 
@@ -76,8 +111,8 @@ public class Player_stats : MonoBehaviour
     public void SaveToJson()
     {
         string json = JsonUtility.ToJson(playerBSC, true);
-        File.WriteAllText(Application.dataPath + "/changeScene.json", json);
-        Debug.Log("Se guardo Exitosamente");
+        // File.WriteAllText(Application.dataPath + "/changeScene.json", json);
+        // Debug.Log("Se guardo Exitosamente");
     }
 
     public Player_basic LoadFromJson()
