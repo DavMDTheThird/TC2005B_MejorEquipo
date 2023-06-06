1cdcd0f83e79299db515bb27899903898f998498
Daniel <a01782985@tec.mx>
2023-06-06 15:21:54 -0600

    Avance página web con animaciónes

---
+++ Pagina Web/js/index0_1.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.statistics_menu', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.controles', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+});
+
+sr.reveal('.imagenComienzo', {
+    rotate:
+    {
+        x:1,
+        y:180,
+    }
+});
+
