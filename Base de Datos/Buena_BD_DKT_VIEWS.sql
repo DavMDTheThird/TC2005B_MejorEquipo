4c55c1524a48a99d7258fdc74c668ce302b758e9
DavidMDD <dm39988@gmail.com>
2023-06-06 19:19:41 -0600

    Base de Datos BUena

---
+++ Base de Datos/Buena_BD_DKT_VIEWS.sql
@@ -0,0 +1,22 @@
+USE darkesttimes_BD;
+
+-- VIEWS
+-- Usuarios con mas partidas completadas
+CREATE VIEW usuarios_juegosGanados AS
+SELECT nombre, juegos_completados FROM darkesttimes_BD.usuarios
+ORDER BY juegos_completados DESC;
+SELECT * FROM usuarios_juegosGanados;
+
+-- Mobs que han matado mas al usuario
+CREATE VIEW mobs_masAsesinatos AS
+SELECT enemigos_nombres.nombre_enemigo, enemigos.asesinatos FROM enemigos_nombres
+JOIN enemigos ON enemigos_nombres.id_enemigo = enemigos.id_enemigo ORDER BY asesinatos DESC;
+SELECT * FROM mobs_masAsesinatos;
+
+-- Usuarios con mas muertes acumuladas
+CREATE VIEW usuario_masMalo AS
+SELECT nombre, muertes_totales FROM darkesttimes_BD.usuarios
+ORDER BY muertes_totales DESC;
+SELECT * FROM usuario_masMalo;
+
+
