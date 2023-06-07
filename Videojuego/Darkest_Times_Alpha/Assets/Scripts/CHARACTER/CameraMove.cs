a54e290afa21e49dac8b5d4d80dbca16948eb8b8
angelafonso227 <angel.adrianafonso22@gmail.com>
2023-06-07 16:34:00 -0600

    Cambios de lobby y camara general

--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/CHARACTER/CameraMove.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/CHARACTER/CameraMove.cs
@@ -4,31 +4,36 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public float followSpeed = 2f;
-    public Transform objectToFollow;
+    public Transform target;
+    public float smoothing;
     public float yOffset = 1f;
+
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
 
     private GameObject[] objectToFollowTAG;
 
     void Start()
     {
-        //Como se inicia el juego en diferentes puntos, buscar al jugador.
         objectToFollowTAG = GameObject.FindGameObjectsWithTag("Player");
-        objectToFollow = objectToFollowTAG[0].transform;
-        //Por si no encuentra ningun GameObject con el tag de: Player 
-        if (objectToFollow == null)
+        target = objectToFollowTAG[0].transform;
+        if (target == null)
         {
             Debug.LogWarning("Target object not found with tag: " + "Player");
         }
-        //Centrar la camara en el personaje
-        transform.position = new Vector3(objectToFollow.position.x, objectToFollow.position.y + yOffset, -10f);
+        transform.position = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
     }
 
-    void Update()
+    private void LateUpdate()
     {
-        //Debug.Log("Position X: " + transform.position.x + " Position Y: " + transform.position.y + " Position Z: " + transform.position.z);
-        Vector3 newPos = new Vector3(objectToFollow.position.x, objectToFollow.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
-    }
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
 
+            targetPosition.x = Mathf.Clamp(target.position.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(target.position.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+        }
+    }
 }
