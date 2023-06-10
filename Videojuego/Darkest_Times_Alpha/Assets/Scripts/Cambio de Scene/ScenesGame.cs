b0c06575a801a172f6f83c4414f40b73b66d4497
Saikou17 <a01783208@itesm.mx>
2023-06-09 22:22:57 -0600

    Puliendo bugs

--- Videojuego/Darkest_Times_Alpha/Assets/Scripts/Cambio de Scene/ScenesGame.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/Scripts/Cambio de Scene/ScenesGame.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public class ScenesGame : MonoBehaviour
-{
-    public string NameScene;
+// public class ScenesGame : MonoBehaviour
+// {
+//     public string NameScene;
 
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(NameScene);
-    }
-}
+//     public void LoadScene(string sceneName)
+//     {
+//         SceneManager.LoadScene(NameScene);
+//     }
+// }
