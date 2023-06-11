2c6df4ae9282813234a76a25427824c95def5e0e
Saikou17 <a01783208@itesm.mx>
2023-06-11 17:33:32 -0600

    Hospital Alpha V2

--- Videojuego/Darkest_Times_Alpha/Assets/ZonaSegura.cs
+++ Videojuego/Darkest_Times_Alpha/Assets/ZonaSegura.cs
@@ -6,10 +6,7 @@ public class ZonaSegura : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D enemigo)
     {
-        Debug.Log("Enemigo.gameobject" + enemigo.gameObject.name);
-        Debug.Log("Enemigo.gameobject" + enemigo.gameObject.tag);
-        if(enemigo.gameObject.CompareTag("Enemigos")){
-            Destroy(enemigo.gameObject);
-        }
+    Debug.Log(enemigo.gameObject.name);
+        Debug.Log(enemigo.gameObject.tag);
     }
 }
