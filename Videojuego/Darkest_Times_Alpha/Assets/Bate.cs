120248567ac9202364259f394a9448c0f5d7b8aa
angelafonso227 <angel.adrianafonso22@gmail.com>
2023-06-13 23:52:51 -0600

    Arma sin balas

---
+++ Videojuego/Darkest_Times_Alpha/Assets/Bate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bate: MonoBehaviour
+{
+    
+
+
+
+}
+//------Cosas a hablar------
+/*Para que el arma no se vaya mas alla del personaje, se tiene que poner el follow speed de la camara instantaneo, practicamente aaaa.*/
