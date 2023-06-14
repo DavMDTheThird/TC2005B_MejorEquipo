004562d3df85e6dcdf65ee6193a68f8c8da78a63
DavidMDD <dm39988@gmail.com>
2023-06-14 13:25:11 -0600

    Add cambios y comentarios a api y mysql

--- Base de Datos/Buena_BD_DKT_VIEWS.sql
+++ Base de Datos/Buena_BD_DKT_VIEWS.sql
@@ -50,18 +50,21 @@ SELECT * FROM darkesttimes_BD.personaje;
 SELECT * FROM darkesttimes_BD.checkpoints;
 SELECT * FROM darkesttimes_BD.inventario;
 
-SELECT *
+SELECT c.id_usuario, c.id_personaje, c.id_nivel
 FROM darkesttimes_BD.checkpoints AS c
 JOIN darkesttimes_BD.personaje AS p ON p.id_personaje = c.id_personaje
 JOIN darkesttimes_BD.usuarios AS u ON c.id_usuario = u.id_usuario
 WHERE u.id_usuario = 10 ORDER BY p.id_personaje DESC LIMIT 1;
+
+-- id_usuario, id_personaje, id_nivel
 
 SELECT p.vida_actual, p.vida_max, p.nivel, p.xp, p.suerte, p.ataque, p.stamina,
        p.inventario, p.multiplicador_monedas, p.monedas
 FROM darkesttimes_BD.checkpoints AS c
 JOIN darkesttimes_BD.personaje AS p ON p.id_personaje = c.id_personaje
 JOIN darkesttimes_BD.usuarios AS u ON c.id_usuario = u.id_usuario
-WHERE u.id_usuario = 1 ORDER BY p.id_personaje DESC LIMIT 1;
+WHERE u.id_usuario = 10 ORDER BY p.id_personaje DESC LIMIT 1;
+
 
 
 
